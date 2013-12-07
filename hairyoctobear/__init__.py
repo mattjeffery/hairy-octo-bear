a486925a2da8d3e440d3e4be0bb1e6953c759536
Matt Jeffery <matt@clan.se>
2013-12-07 15:48:02 +0000

    refactored api proxy endpoint

--- hairyoctobear/__init__.py
+++ hairyoctobear/__init__.py
@@ -1,13 +1,20 @@
 import os
+from dogpile.cache import make_region
 from wsgicors import CORS
-from pyramid.config import Configurator
+from pyramid.config import Configurator, ConfigurationError
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    if "SEMETRIC_API" in os.environ:
+
+    try:
         settings["api.token"] = os.environ["SEMETRIC_API"]
+        settings["memcache.password"] = os.environ["MEMCACHEDCLOUD_PASSWORD"]
+        settings["memcache.servers"] = os.environ["MEMCACHEDCLOUD_SERVERS"]
+        settings["memcache.user"] = os.environ["MEMCACHEDCLOUD_USERNAME"]
+    except KeyError as exc:
+        raise ConfigurationError("Failed to load config from env: {0}".format(exc))
 
     config = Configurator(settings=settings)
     config.add_route('proxy', '/*url')
@@ -15,7 +22,19 @@ def main(global_config, **settings):
 
     config.include("hairyoctobear.api")
 
-    app = config.make_wsgi_app()
+    registry = config.registry
+
+    registry._cache_region = make_region().configure(
+        'dogpile.cache.bmemcached',
+        expiration_time = 3600,
+        arguments = {
+            'url': settings["memcache.servers"].split(),
+            'username':settings["memcache.user"],
+            'password':settings["memcache.password"]
+        }
+    )
+
+    registry.get_or_create = registry._cache_region.get_or_create
 
     return CORS(config.make_wsgi_app(),
                 headers="*",
--- hairyoctobear/views.py
+++ hairyoctobear/views.py
@@ -8,24 +8,33 @@ from pyramid.view import view_config
 log = logging.getLogger(__name__)
 
 
+def _api_request(api_path, api_token, args):
+    """
+    Proxys a request to the semetric api
+    :param api_path: api path on the semetric api
+    :param api_token: semetric api token
+    :param args: api args
+    """
+    url = "http://app.musicmetric.com/api/{path}".format(path=api_path)
+    if "token" not in args:
+        args["token"] = api_token
+    if args:
+        url += "?" + urlencode(args)
+    log.debug("Proxying request to {0}".format(url))
+    status, resp = httplib2.Http().request(url)
+    return json.loads(resp)
+
+
 @view_config(route_name='proxy', renderer='jsonp')
 def api_proxy(request):
-    http = httplib2.Http()
     log.debug("Matched: {0}".format(request.matchdict))
     api_path = '/'.join(request.matchdict.get("url"))
-    url = "http://app.musicmetric.com/api/{path}".format(path=api_path)
 
     # add the query arguments
     args = dict(request.params)
     if "_method" not in args:
         args["_method"] = request.method
 
-    if "token" not in args:
-        args["token"] = request.registry.settings["api.token"]
+    api_token = request.registry.settings["api.token"]
 
-    if args:
-        url += "?"+urlencode(args)
-    log.debug("Proxying request to {0}".format(url))
-    status, resp = http.request(url)
-
-    return json.loads(resp)
+    return _api_request(api_path, api_token, args)
