521eb0d185f768c2c0532fdc15413d129e2bc09b
Matt Jeffery <matt@clan.se>
2013-12-07 14:21:01 +0000

    Added API proxy

--- hairyoctobear/__init__.py
+++ hairyoctobear/__init__.py
@@ -1,10 +1,14 @@
+import os
 from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    if "SEMETRIC_API" in os.environ:
+        settings["api.token"] = os.environ["SEMETRIC_API"]
+
     config = Configurator(settings=settings)
-    config.add_route('home', '/api/')
+    config.add_route('proxy', '/*url')
     config.scan()
     return config.make_wsgi_app()
--- hairyoctobear/views.py
+++ hairyoctobear/views.py
@@ -1,6 +1,31 @@
+import json
+from urllib import urlencode
+import httplib2
+import logging
+
 from pyramid.view import view_config
 
+log = logging.getLogger(__name__)
 
-@view_config(route_name='home', renderer='json')
-def my_view(request):
-    return {'project': 'hairyoctobear'}
+
+@view_config(route_name='proxy', renderer='json')
+def api_proxy(request):
+    http = httplib2.Http()
+    log.debug("Matched: {0}".format(request.matchdict))
+    api_path = '/'.join(request.matchdict.get("url"))
+    url = "http://app.musicmetric.com/api/{path}".format(path=api_path)
+
+    # add the query arguments
+    args = dict(request.params)
+    if "_method" not in args:
+        args["_method"] = request.method
+
+    if "token" not in args:
+        args["token"] = request.registry.settings["api.token"]
+
+    if args:
+        url += "?"+urlencode(args)
+    log.debug("Proxying request to {0}".format(url))
+    status, resp = http.request(url)
+
+    return json.loads(resp)
